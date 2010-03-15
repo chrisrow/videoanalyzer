3009e7d3af5a61dc7fe29a0525dee6e330ba80bb
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-03-15 03:17:57 +0000

    增加录像功能

--- VideoAnalyzer/src/VideoAnalyzer/Subject.h
+++ VideoAnalyzer/src/VideoAnalyzer/Subject.h
@@ -1,7 +1,26 @@
 
 #pragma once
 
+#include "afxmt.h"
 #include <vector>
+#include <list>
+
+class CGuard
+{
+public:
+    CGuard(CCriticalSection& CSect): m_lock(&CSect)
+    { 
+        m_lock.Lock(); 
+    }
+
+    ~CGuard()                 
+    { 
+        m_lock.Unlock(); 
+    }
+
+private:
+    CSingleLock m_lock;
+};
 
 // Obersver模式中的Subject基类
 // 注意： 类型T必须是一个指针
@@ -13,58 +32,130 @@ public:
 
     void addListener(T elem);
     void removeListener(T elem);
+    void retrieveListener();
+
     void clearListener();
     void destroyListener();
 
 protected:
     std::vector<T> m_elem;  //
+    std::list<T>   m_tmpAddElem;
+    std::list<T>   m_tmpRemElem;
+
+    CCriticalSection m_CSect;
 };
 
 template<typename T>
 void CSubject<T>::addListener(T elem)
 {
-    std::vector<T>::iterator it = m_elem.begin();
-    for (; it != m_elem.end(); it++)
+    if (elem != NULL)
     {
-        if (elem == *it)
-        {
-            return;
-        }
-
-        if (NULL == *it)
-        {
-            *it = elem;
-            return;
-        }
+        CGuard guard(m_CSect);
+        m_tmpAddElem.push_back(elem);
     }
-    m_elem.push_back(elem);
 }
 
 template<typename T>
 void CSubject<T>::removeListener(T elem)
 {
-    std::vector<T>::iterator it = m_elem.begin();
-
-    for (; it != m_elem.end(); it++)
+    if (elem != NULL)
     {
-        if (elem == *it)
-        {
-            *it = NULL;
-        }
+        CGuard guard(m_CSect);
+        m_tmpRemElem.push_back(elem);
     }
 }
 
 template<typename T>
+void CSubject<T>::retrieveListener()
+{
+    CGuard guard(m_CSect);
+
+    std::list<T>::iterator itToAdd = m_tmpAddElem.begin();
+    for (; itToAdd != m_tmpAddElem.end(); itToAdd++)
+    {
+        std::vector<T>::iterator it = m_elem.begin();
+        for (; it != m_elem.end(); it++)
+        {
+            if (*itToAdd == *it)
+            {
+                break;
+            }
+            else if (NULL == *it)
+            {
+                *it = *itToAdd;
+                break;
+            }
+        }
+        if (it == m_elem.end())
+        {
+            m_elem.push_back(*itToAdd);
+        }
+    }
+    m_tmpAddElem.clear();
+
+    std::list<T>::iterator itToRem = m_tmpRemElem.begin();
+    for (; itToRem != m_tmpRemElem.end(); itToRem++)
+    {
+        std::vector<T>::iterator it = m_elem.begin();
+        for (; it != m_elem.end(); it++)
+        {
+            if (*itToRem == *it)
+            {
+                *it = NULL;
+                break;
+            }
+        }
+    }
+    m_tmpRemElem.clear();
+}
+
+// template<typename T>
+// void CSubject<T>::addListenerT elem)
+// {
+//     std::vector<T>::iterator it = m_elem.begin();
+//     for (; it != m_elem.end(); it++)
+//     {
+//         if (elem == *it)
+//         {
+//             return;
+//         }
+// 
+//         if (NULL == *it)
+//         {
+//             *it = elem;
+//             return;
+//         }
+//     }
+//     m_elem.push_back(elem);
+// }
+// 
+// template<typename T>
+// void CSubject<T>::removeListener(T elem)
+// {
+//     std::vector<T>::iterator it = m_elem.begin();
+//     for (; it != m_elem.end(); it++)
+//     {
+//         if (elem == *it)
+//         {
+//             *it = NULL;
+//         }
+//     }
+// }
+
+template<typename T>
 void CSubject<T>::clearListener()
 {
+//     CGuard guard(m_CSect);
+
     m_elem.clear();
 }
 
 template<typename T>
 void CSubject<T>::destroyListener()
 {
-    std::vector<T>::iterator it = m_elem.begin();
+//     CGuard guard(m_CSect);
 
+    std::vector<T>::iterator it = m_elem.begin();
     for (; it != m_elem.end(); it++)
     {
         delete *it;
@@ -73,6 +164,7 @@ void CSubject<T>::destroyListener()
 }
 
 #define FOR_EACH(type, func, ...) \
+{\
     std::vector<##type>::iterator it = m_elem.begin();\
     for (; it != m_elem.end(); it++)\
     {\
@@ -80,4 +172,5 @@ void CSubject<T>::destroyListener()
         {\
             (*it)->##func(__VA_ARGS__);\
         }\
-    }
+    }\
+}
