883ee757f767430da0d348294b3e6b3590ee965b
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-03-15 08:43:58 +0000

    解决播放视频文件时无法显示图像的问题

--- trunk/VideoAnalyzer/src/VideoAnalyzer/VideoGraber.cpp
+++ trunk/VideoAnalyzer/src/VideoAnalyzer/VideoGraber.cpp
@@ -434,6 +434,7 @@ void CVideoFile::doRun()
 
         if (m_uFrame >= m_iStartFrame)
         {
+            retrieveListener();
             FOR_EACH(IFrameReceiver*, updateFrame, pFrame);
             cvWaitKey(1);
             Sleep(m_iWaitTime);
