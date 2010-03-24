dce6877b5924a0d4fe684c1dbff2d48fb43d58fb
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-03-24 08:46:41 +0000

    编译通过，能运行

--- src/Algorithm/PersonWarpper.cpp
+++ src/Algorithm/PersonWarpper.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "PersonWarpper.h"
 #include "CPersonDetect.h"
+#include "VideoAnalyzer/Line.h"
 
 CPersonWarpper::CPersonWarpper()
-: m_pFrameContainer(NULL)
+: m_RGB_template(NULL)
+, m_pFrameContainer(NULL)
 , m_pOutFrameContainer(NULL)
 , m_pFrame_matlabFunced(NULL)
 , m_pDetector(NULL)
@@ -18,11 +20,13 @@ CPersonWarpper::~CPersonWarpper()
 
 void CPersonWarpper::reset()
 {
+    delete m_RGB_template;
     delete m_pFrameContainer;
     delete m_pOutFrameContainer;
     delete m_pFrame_matlabFunced;
     delete m_pDetector;
 
+    m_RGB_template = NULL;
     m_pFrameContainer = NULL;
     m_pOutFrameContainer = NULL;
     m_pFrame_matlabFunced = NULL;
@@ -42,12 +46,22 @@ const IplImage* CPersonWarpper::analysis(const IplImage *pFrame)
 		m_pFrameContainer = new CFrameContainer(pFrame->width, pFrame->height);
 		m_pOutFrameContainer = new CFrameContainer(pFrame->width, pFrame->height);
 		m_pFrame_matlabFunced = new CFrameContainer(pFrame->width, pFrame->height);
-	}
+
+        m_RGB_template = new unsigned char[pFrame->width * pFrame->height];
+        Line* getTemplate = new Line(pFrame->width, pFrame->height);
+        bool b_get_template_is_OK = getTemplate->GetTemplateParameter(m_RGB_template,
+            _T("\\set_TestGUIDLG.set"), pFrame->width, pFrame->height);
+        if(!b_get_template_is_OK)
+        {
+            MessageBoxA( NULL, "模板参数读取失败!", "Line", MB_OK );
+        }
+        delete getTemplate;
+    }
 
 	memcpy(m_pFrameContainer->m_BmpBuffer, pFrame->imageData, pFrame->imageSize);
 
-    // TODO: 人员检测总入口
-// 	m_pDetector->ParaDetectTwo(m_pFrameContainer, m_pOutFrameContainer); //jojo
+    m_pDetector->PersenDetect_Process_Channel_3(m_pFrame_matlabFunced, m_pOutFrameContainer,
+        m_pFrameContainer, m_RGB_template, m_pDetector->LEFTTORIGNT, 60/*m_nInputFrameNum*/); 
 
 	return m_pOutFrameContainer->getImage();
 }
@@ -59,12 +73,12 @@ ALERTTYPE CPersonWarpper::haveAlert()
 
 void CPersonWarpper::updateFrame(const IplImage *pFrame)
 {
-    const IplImage* p = this->analysis(pFrame) ;
+    const IplImage* p = analysis(pFrame) ;
 
     if ( this->haveAlert() != AT_NONE)
     {
         //生成报警截图
-//         m_pDetector->ImgMoveObjectDetect(m_pFrameContainer, m_pFrame_matlabFunced );
+        m_pDetector->ImgMoveObjectDetect(m_pFrameContainer);
 
         //报警
         FOR_EACH(IAlerter*, alert, m_pFrame_matlabFunced->getImage());
