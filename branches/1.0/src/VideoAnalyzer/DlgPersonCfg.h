881de9a137eb286afc36c30650c672a771db7b20
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-03-31 03:07:03 +0000

    增强绘图控件功能

--- branches/1.0/src/VideoAnalyzer/DlgPersonCfg.h
+++ branches/1.0/src/VideoAnalyzer/DlgPersonCfg.h
@@ -3,6 +3,7 @@
 #include "MaskStatic.h"
 #include "cv.h"
 #include "highgui.h"
+#include "afxwin.h"
 
 // CDlgPersonCfg 对话框
 
@@ -30,8 +31,19 @@ private:
     LineArray m_warningLine;   //预警线
     RectArray m_rect;
 
+    CButton m_btnWarnLine;
+    CButton m_btnMask;
+    CButton m_btnFill;
+
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnBnClickedButtonWarningLine();
+    afx_msg void OnBnClickedButtonClearWarningLine();
+    afx_msg void OnBnClickedButtonPoly();
+    afx_msg void OnBnClickedButtonClearPoly();
+    afx_msg void OnBnClickedButtonFill();
+    afx_msg void OnBnClickedButtonClearall();
 };
