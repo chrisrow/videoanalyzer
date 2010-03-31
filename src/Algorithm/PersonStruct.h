39014d16bbc087abccb848dfe914adc4904d9f02
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-03-31 14:23:53 +0000

    又完善了一下读配置文件的功能。还存在以下问题： 1、在读取配置文件后自动产生遮罩图像 2、中间图像上绘制的预警线还有问题

--- src/Algorithm/PersonStruct.h
+++ src/Algorithm/PersonStruct.h
@@ -11,9 +11,19 @@ typedef std::vector<PolyLine> PolyLineArray;
 //人员检测相关参数
 struct TPersonDetect
 {
-    Line warnLing;
+    Line warnLine;
     PolyLineArray maskLine;
     IplImage* mask;
 
     TPersonDetect(): mask(NULL) {}
+    void reset()
+    {
+        warnLine.clear();
+        maskLine.clear();
+        if (mask)
+        {
+            cvReleaseImage(&mask);
+            mask = NULL;
+        }
+    }
 };
