74a90c09863d4f560d17419d0232f0c39b4ad14c
thorqq <thorqq@cd4f8544-12f3-11df-bbc9-e504a80a8618>
2010-04-01 13:25:19 +0000

    重写读取xml中全局参数的代码

---
+++ src/VideoAnalyzer/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+struct TCommonParam
+{
+    unsigned char szLocalAddr[4];
+    unsigned char szUDPServerIP[4];
+    int iUDPServerPort;
+    char szImagePath[MAX_PATH+1];
+    int iHeartBeat;
+
+    TCommonParam();
+
+    void reset();
+};
+
+const char* loadCommonParam(const char* szFileName);
